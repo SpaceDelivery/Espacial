eb306058f10bcd3f528093b706e92ede316cb24f
DorianPadilla <25dorian05@gmail.com>
2023-05-09 09:59:18 -0500

    Base

--- src/Productos.js
+++ src/Productos.js
@@ -1,37 +1,27 @@
 import React from 'react'
-import button from'./button.js'
+
 import './Style/productos.css';
 export default function Productos() {
-  EnviarCarrati(){
-    var  id = [ ]
-
-  }
   return (
-    <section className="conteiner-prod">
-            <div className="tarjet-1" id='1'>
-              <div className='text-v'>
-                  <div><h1> ¡VENDO¡</h1>
-                  <img className='img-producto' src="./assets/P-1.jpg" alt="logo"></img>
-              </div>
+    <div className='conteiner'>
+      <section className="conteiner-prod1">
+            <article className="tarjet-1" id='1'>
+              <div><h1> ¡VENDO¡</h1>
+                    <img className='img-producto' src="./assets/P-1.jpg" alt="logo"></img>
+              </div>    
+                <div className='text-v'><p> Se vende un PS4 en excelentes condiciones, con poco uso y completamente funcional. Incluye un control inalámbrico DualShock 4 y todos los cables necesarios. Ofrece una amplia variedad de juegos, películas y música en línea con gráficos impresionantes y un rendimiento fluido. El PS4 ha sido bien cuidado, restaurado de fábrica y no tiene rasguños o marcas en la superficie. Está listo para ser usado y proporcionará horas de entretenimiento. Si buscas una consola de juegos en excelentes condiciones, este PS4 es perfecto para ti. ¡No pierdas la oportunidad de adquirir este maravilloso sistema de entretenimiento en casa!</p>
                 </div>
-                <p> Se vende consola PS4 en excelentes condiciones, con poco tiempo de uso y completamente funcional. La consola viene con un control inalámbrico DualShock 4 y todos los cables necesarios para su uso. 
-                El PS4 es un sistema de entretenimiento en casa que ofrece una amplia variedad de juegos, películas y música en línea. Con gráficos increíbles y un rendimiento fluido, esta consola proporciona una experiencia de juego única y envolvente.
 
-                Este PS4 ha sido bien cuidado y se encuentra en excelentes condiciones. Ha sido utilizado de forma moderada y se encuentra en perfecto estado, sin ningún tipo de rasguños o marcas en su superficie. 
-
-                Se ha restaurado de fábrica para asegurar que esté libre de cualquier tipo de problema técnico. Este PS4 está listo para ser usado inmediatamente y ofrecerá horas de entretenimiento a su nuevo propietario.
-
-                Si estás buscando una consola de juegos que ofrezca una experiencia de juego increíble y esté en excelentes condiciones, ¡este PS4 es perfecto para ti! No te pierdas la oportunidad de adquirir este maravilloso sistema de entretenimiento en casa.</p>
-              
-        
-              <input  className="button-buy"type='button' value="Agregar al carrito" ></input>
-            </div>
-            <div className="tarjet-2">
-              
-              <div className='text-v'>
-                <p>  </p>
+                <input  className="button-buy"type='submit' value="Agregar al carrito" ></input>
+            </article>    
+      </section>
+      <section className="conteiner-prod2"> 
+      <div className="tarjet-2">
+                <img className='img-producto' src='./assets/P-2.jpg' alt='Producto'></img>
               </div>
-            </div>    
-    </section>
-  )
+                <div className='text-v'>
+                  <p> Venta: PC Mac con 1 mes de uso. Incluye procesador Apple M1, 8GB RAM, 256GB de almacenamiento SSD y pantalla de 13". Perfecta para trabajo y estudio. Precio: menos de $400. ¡Aprovecha esta oportunidad única! </p>
+                </div></section>
+    </div> 
+    )
 }
